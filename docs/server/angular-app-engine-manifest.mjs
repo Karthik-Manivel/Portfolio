1462eff2a5549194c5e8cad046e854ec127160aa
Karthik Manivel <kmanive1@for.com>
2025-02-14 23:11:27 +0530

    commit

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/upshotengineering',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/upshotengineering/',
+  baseHref: '/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 515, hash: 'ee13bedcc5ab167490a512e5b5dfa3a378e7ec4466dbdac18deac55b704cd573', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1028, hash: '8e2ac107cfdee54310efdb1679625cdb349af369ff09edf5793da465b77f209d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 497, hash: 'ba8e0bf69a66d978cf889f0272bb9aac6e1a06c90b26648b388fffc19b38a9b3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1010, hash: '73d9fe07425f9ca84a4a85719a3362e225f1b5a299499eb8480779a071cce4f9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
