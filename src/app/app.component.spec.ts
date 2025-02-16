ec8aebf34448bbe4eca20dc4c10e5d472560b927
Karthik Manivel <kmanive1@for.com>
2025-02-16 15:42:01 +0530

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'upshot2'`, () => {
+  it(`should have as title 'upshot'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('upshot2');
+    expect(app.title).toEqual('upshot');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('upshot2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('upshot app is running!');
   });
 });
